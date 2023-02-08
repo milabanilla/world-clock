bac0da6e49e3afa9c63f00ecfd015d5ee2e14146
Mila <yamila_meza_@outlook.com>
2023-02-08 10:05:39 -0800

    Added a function to give current date and time for cities

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,18 @@
 //Los angeles
-
+function updateTime(){
 let losAngelesElement = document.querySelector("#los-angeles")
 let losAngelesDateElement= losAngelesElement.querySelector(".date")
 let losAngelesTimeElement= losAngelesElement.querySelector(".time")
-losAngelesDateElement.innerHTML= moment().format("MMMM DD YYYY")
-losAngelesTimeElement.innerHTML= moment().format("h:mm:ss")
+let losAngelesTime = moment().tz("America/Los_Angeles");
+losAngelesDateElement.innerHTML= losAngelesTime.format("MMMM DD, YYYY ")
+losAngelesTimeElement.innerHTML= losAngelesTime.format("h:mm:ss [<small>]A[</small>]")
+//Tokyo
+let tokyoElement = document.querySelector("#tokyo")
+let tokyoDateElement= tokyoElement.querySelector(".date")
+let tokyoTimeElement= tokyoElement.querySelector(".time")
+let tokyoTime = moment().tz("Asia/Tokyo");
+tokyoDateElement.innerHTML= tokyoTime.format("MMMM DD, YYYY ")
+tokyoTimeElement.innerHTML= tokyoTime.format("h:mm:ss [<small>]A[</small>]")
+}
+
+setInterval(updateTime,1000);
